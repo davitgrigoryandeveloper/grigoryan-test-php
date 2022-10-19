94ef9de6c5850df2fd10fdc24465359788c5b470
Davit Grigoryan <davit.grigoryan@esterox.am>
2022-10-19 17:49:12 +0400

    built a simple PHP MVC framework

---
+++ Simple-PHP-MVC-Framework/README.md
@@ -0,0 +1,10 @@
+MVC Framework Architecture
+======
+
+## Installation via Composer([packagist](https://packagist.org/packages/hhxsv5/php-sse))
+
+```BASH
+composer dump-autoload
+composer require symfony/routing
+composer require symfony/http-foundation
+```
---
+++ Simple-PHP-MVC-Framework/config/config.php
@@ -0,0 +1,13 @@
+<?php
+//site name
+const SITE_NAME = 'your-site-name';
+
+//App Root
+define('APP_ROOT', dirname(__FILE__, 2));
+const URL_ROOT = '/';
+
+//DB Params
+const DB_HOST = 'host';
+const DB_USER = 'username';
+const DB_PASS = 'password';
+const DB_NAME = 'db_name';
