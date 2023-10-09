f4c00a0ee932c0f69e27ee1d6d539ba74f476938
Davit Grigoryan <davit.grigoryan@esterox.am>
2023-10-09 09:20:24 +0400

    code improvement

--- Algorythm/BfsGraphAlgorythm.php
+++ Algorythm/BfsGraphAlgorythm.php
@@ -21,7 +21,7 @@ function bfs_shortest_path($graph, $start, $end) {
         }
 
         // Visit all the neighbors of the current node
-        foreach (array_keys($graph[$current], true) as $neighbor) {
+        foreach ($graph[$current] as $neighbor) {
             if (!$visited[$neighbor]) {
                 $visited[$neighbor] = true;
                 $previous[$neighbor] = $current;
@@ -32,4 +32,24 @@ function bfs_shortest_path($graph, $start, $end) {
 
     // If we reach here, there is no path from start to end
     return null;
+}
+
+$graph = array(
+    0 => array(1, 2),      // Node 0 is connected to nodes 1 and 2
+    1 => array(0, 3, 4),   // Node 1 is connected to nodes 0, 3, and 4
+    2 => array(0, 4),      // Node 2 is connected to nodes 0 and 4
+    3 => array(1),         // Node 3 is connected to node 1
+    4 => array(1, 5),      // Node 4 is connected to nodes 1 and 5
+    5 => array(2, 4)       // Node 5 is connected to nodes 2 and 4
+);
+
+$start = 0;  // Start node
+$end = 5;    // End node
+
+$path = bfs_shortest_path($graph, $start, $end);
+
+if ($path === null) {
+    echo "No path found from node $start to node $end.";
+} else {
+    echo "Shortest path from node $start to node $end: " . implode(" -> ", $path);
 }
